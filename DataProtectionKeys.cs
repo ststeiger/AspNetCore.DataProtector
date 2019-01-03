a44e4f1445f709c4d763806036b7260d391fd35b
韩基伟 <amh1979@qq.com>
2019-01-03 21:47:39 +0800

    init

---
+++ DataProtectionKeys.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+namespace AspNetCore.DataProtector
+{
+    internal class DataProtectionKeys
+	{
+        public Guid Id { get; set; }
+        public DateTime CreationDate { get; set; }
+		public DateTime ExpirationDate { get; set; }
+		public byte[] MasterKey { get; set; }
+        
+		[Newtonsoft.Json.JsonIgnore]
+		public bool IsRevoked { get { return ExpirationDate < DateTime.Now; } }
+	}
+}
