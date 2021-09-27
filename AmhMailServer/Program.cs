03c0677219a247ef64d9c7f51fb1d5c025f61db8
ststeiger <quandary82@hailmail.net>
2021-09-27 17:28:52 +0200

    Add Module

--- AmhMailServer/Program.cs
+++ AmhMailServer/Program.cs
@@ -4,6 +4,17 @@ namespace AmhMailServer
 {
     class Program
     {
+
+        // (await navigator.storage.estimate()).usage
+        // (await navigator.storage.estimate()).quota/(1024*1024*1024)
+
+        // https://love2dev.com/blog/what-is-the-service-worker-cache-storage-limit/
+        // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API
+        // https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
+        // https://developer.mozilla.org/en-US/docs/Web/API/Service_Worker_API/Using_Service_Workers
+        // https://medium.com/@onejohi/offline-web-apps-using-local-storage-and-service-workers-5d40467117b9
+        // https://www.sitepoint.com/offline-web-apps-service-workers-pouchdb/
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
